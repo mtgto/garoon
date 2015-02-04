998f2a5d7f16d80991ead395daa845c9b69bb2c1
mtgto <hogerappa@gmail.com>
2015-02-05 00:52:25 +0900

    通知を完了にする関数を追加．

--- src/main/scala/net/mtgto/garoon/notification/NotificationService.scala
+++ src/main/scala/net/mtgto/garoon/notification/NotificationService.scala
@@ -1,7 +1,7 @@
 package net.mtgto.garoon.notification
 
 import com.github.nscala_time.time.Imports._
-import net.mtgto.garoon.{Authentication, GaroonClient}
+import net.mtgto.garoon.{RequestToken, Authentication, GaroonClient}
 
 import scala.util.Try
 import scala.xml.XML
@@ -32,6 +32,26 @@ class NotificationService(client: GaroonClient, auth: Authentication) {
     }
 
     val result = client.sendReceive(actionName, "/cbpapi/notification/api", parameters)(auth, None)
+    result.map { element =>
+      val node = XML.loadString(element.toString)
+      (node \ "returns" \ "notification").map(Notification(_))
+    }
+  }
+
+  // 通知を完了にする
+  def checkNotifications(notificationIds: Seq[Tuple2[ModuleId, String]])
+      (requestToken: RequestToken): Try[Seq[Notification]] = {
+    val actionName = "NotificationConfirmNotification"
+    val parameters = client.factory.createOMElement("parameters", null)
+    notificationIds.foreach {
+      case (moduleId: ModuleId, item: String) =>
+        val notificationNode = client.factory.createOMElement("notification_id", null)
+        notificationNode.addAttribute("module_id", moduleId.value, null)
+        notificationNode.addAttribute("item", item, null)
+        parameters.addChild(notificationNode)
+    }
+
+    val result = client.sendReceive(actionName, "/cbpapi/notification/api", parameters)(auth, Some(requestToken))
     result.map { element =>
       val node = XML.loadString(element.toString)
       (node \ "returns" \ "notification").map(Notification(_))
